758d5b09c71b7efd4fe61f106fc3d7cda34493c9
FenerliYgt <ygt1618@gmail.com>
2023-12-27 21:16:57 +0300

    Create main2.py

---
+++ main2.py
@@ -0,0 +1,21 @@
+a=sınav1=float(input("1. Sınav Notunu Gir:"))
+b=sınav2=float(input("2. Sınav Notunu Gir:"))
+c=sınav3=float(input("3. Sınav Notunu Gir:"))
+d=(a+b+c)//3
+print(float(d))
+
+if d>=50:
+    print("Öğrenci Dersi Geçti.")
+    print("Öğrenci Başarılı.")
+
+elif d<50:
+    print("Öğrenci Dersi Geçemedi.")
+    print("Öğrenci Başarısız.")
+
+
+
+
+
+
+
+    
